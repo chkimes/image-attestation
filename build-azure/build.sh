dfdfa4abbab9b9c399cf5a93b0a09a0255490d57
Chad Kimes <chkimes@github.com>
2024-04-03 16:44:18 +0000

    Add a bit more logging

--- build-azure/build.sh
+++ build-azure/build.sh
@@ -10,6 +10,8 @@ fi
 
 az account set --subscription $AZURE_SUBSCRIPTION_ID
 az group create --resource-group $AZURE_RESOURCE_GROUP --location $AZURE_LOCATION
+
+echo "Creating VM..."
 az vm create --resource-group $AZURE_RESOURCE_GROUP \
              --name $AZURE_VM_NAME \
              --image Canonical:0001-com-ubuntu-minimal-mantic:minimal-23_10-gen2:23.10.202402260 \
@@ -20,9 +22,8 @@ az vm create --resource-group $AZURE_RESOURCE_GROUP \
              --os-disk-delete-option delete \
              | tee create.log
 
-
 cleanup() {
-    echo "Cleaning up"
+    echo "Cleaning up..."
     az vm delete --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_VM_NAME --yes
     rm -f create.log
 }
@@ -32,10 +33,13 @@ trap 'cleanup' EXIT
 IP_ADDR=$(cat create.log | jq -r .publicIpAddress | tail -n 1)
 echo "VM created with IP address: $IP_ADDR"
 
-echo "Waiting for public key to load into VM"
+echo "Waiting for public key to load into VM..."
 sleep 60
 
+echo "Copying files to VM..."
 scp -r -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa "$SCRIPTPATH/../initramfs" azureuser@$IP_ADDR:
 scp    -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa "$SCRIPTPATH/build-vm.sh"  azureuser@$IP_ADDR:
+
+echo "Building VM image..."
 ssh    -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa azureuser@$IP_ADDR "sudo ./build-vm.sh"
 scp    -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa azureuser@$IP_ADDR:~/image.tar.gz .
